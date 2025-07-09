fdf07d657ab1687b98e4bee8ec17bf3a86063f0e
Falin Pavel <falinpavel96@gmail.com>
2025-07-09 12:01:11 +0300

    my commit

--- test_conflict.py
+++ test_conflict.py
@@ -1,5 +1,5 @@
 def greeting(name):
-    print(f"Hello {name}")
+    print(f"Hello {name}, how are you?")
 
 
 greeting("John")
---
+++ test_conflict.py~
@@ -0,0 +1,5 @@
+def greeting(name):
+    print(f"Hello {name}")
+
+
+greeting("John")
